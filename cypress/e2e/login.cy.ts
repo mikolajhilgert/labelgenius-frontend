75b03a6e41e9a5e82b814ad09c5f7ecc9877b19b
Mikolaj Hilgert <mikolaj.hilgert@protonmail.com>
2024-01-17 10:34:59 +0100

    Refactor login test to include wait and assertion

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,11 +1,12 @@
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+describe("Login test", () => {
+  it("We test the login", () => {
     cy.visit("https://labelgenius.vercel.app/login");
     cy.get("#email").clear();
     cy.get("#email").type("stroxvfx@gmail.com");
     cy.get("#password").clear();
     cy.get("#password").type("abc123456");
     cy.get("[data-cy=button]").click();
+    cy.wait(1000);
+    cy.get("[data-cy=button]").should("not.exist");
   });
 });
