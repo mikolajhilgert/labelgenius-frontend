66e0ec98697fff06a655bac4a85f34eceb52e0a3
Mikolaj Hilgert <mikolaj.hilgert@protonmail.com>
2024-01-19 20:27:19 +0100

    Refactor deleteAccount function to use fetch instead of axios

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -1,15 +1,14 @@
-import axios from "axios";
-
 const API_URL = process.env.REACT_APP_DOMAIN + "/api/me/";
 
 export const deleteAccount = async () => {
   try {
-    const response = await axios.get(API_URL + "delete", {
-      withCredentials: true,
+    const response = await fetch(API_URL + "delete", {
+      method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
+      credentials: "include",
     });
     if (response.status === 200) {
       window.location.href = "/login";
